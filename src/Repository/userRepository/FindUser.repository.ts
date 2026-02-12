d297fd8ae8cf76d445956b9b68c3e0bfe6e5fa2e
ritik1510 <kr.ritik1510@gmail.com>
2026-02-12 16:44:32 +0530

    Add the find user by id repository to find users in db by using its ids

---
+++ src/Repository/userRepository/FindUser.repository.ts
@@ -0,0 +1,11 @@
+import { User } from "@/models/User.models.js"
+
+export const findUserByID_Repository = {
+  findByClerkUserId: async (clerkUserId: string) => {
+    return User.findOne({ clerkUserId }).lean();
+  },
+
+  findById: async (id: string) => {
+    return User.findById(id).lean();
+  },
+}
