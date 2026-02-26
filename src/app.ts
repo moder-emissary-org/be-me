11e1955d8d7c34e6e124e1986e1a0bf365cde283
ritik1510 <kr.ritik1510@gmail.com>
2026-02-26 16:08:50 +0530

    add more complete routes to the backend and setup the test routes for every controller.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
-import systemRouter from "@/routes/system/System.routes.js"
-import userRouter  from "@/routes/User/User.routes.js";
-import healthRouter from "@/routes/Health/Health.routes.js";
-import societyRouter from "@/routes/Society/Society.routes.js";
+import { systemRouter } from "@/routes/system/System.routes.js"
+import { userRouter } from "@/routes/User/User.routes.js";
+import { healthRouter } from "@/routes/Health/Health.routes.js";
+import { societyRouter } from "@/routes/Society/Society.routes.js";
+import { apartmentRouter } from "./routes/Apartment/Apartment.routes.js";
+import { VisitorRouter } from "./routes/Visitor/visitor.routes.js";
+import { complaintRouter } from "./routes/Complaint/Complaint.routes.js";
+import { noticeRouter } from "./routes/Notice/Notice.routes.js";
 
 const app: express.Application = express();
 
@@ -26,9 +30,9 @@ app.use("/api/v1/healthcheck", healthRouter);
 app.use("/api/v1/system", systemRouter)
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/societies", societyRouter);
-// app.use("/api/v1/apartments", apartmentRouter);
-// app.use("/api/v1/visitors", visitorRouter);
-// app.use("/api/v1/complaints", complaintRouter);
-// app.use("/api/v1/notices", noticeRouter);
+app.use("/api/v1/apartments", apartmentRouter);
+app.use("/api/v1/visitors", VisitorRouter);
+app.use("/api/v1/complaints", complaintRouter);
+app.use("/api/v1/notices", noticeRouter);
 
 export { app };
