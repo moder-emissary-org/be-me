c1e557eb25668a9324d59e377c19e50ec02fa926
ritik1510 <kr.ritik1510@gmail.com>
2026-03-22 20:19:15 +0530

    update the createNormalUser

--- src/Repository/userRepository/SaveUser.repository.ts
+++ src/Repository/userRepository/SaveUser.repository.ts
@@ -1,12 +1,19 @@
 import { User } from "@/models/User.models.js";
+import type { Types } from "mongoose";
 
-export const SaveUser_Repository = async (userData: any) => {
+interface createUserInput {
+  clerkUserId: string;
+  fullName: string;
+  email: string; 
+  role: "admin" | "resident" | "guard";
+  societyId: Types.ObjectId;
+  apartmentId?: Types.ObjectId | null; // optional, only for residents
+  isActive: boolean;
+}
 
-  const user = await User.create(
-    userData
-  );
-
-  console.log("Saved user repository layer hit: ", user);
-  
-  return user;
+export const saveUser_Repository = {
+  createNormalUser: async (userData: createUserInput) => {
+    const user = await User.create(userData);
+    return user;
+  }   
 }
