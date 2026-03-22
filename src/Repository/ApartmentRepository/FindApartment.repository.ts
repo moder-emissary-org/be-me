1f05145b4894d42834779f0f652ad811216dedd5
ritik1510 <kr.ritik1510@gmail.com>
2026-03-22 20:15:18 +0530

    add new method to find apartment with society.

--- src/Repository/ApartmentRepository/FindApartment.repository.ts
+++ src/Repository/ApartmentRepository/FindApartment.repository.ts
@@ -1,6 +1,11 @@
 import { Apartment } from "@/models/Apartment.models.js";
 import type { Types } from "mongoose";
 
-export const FindApartment_repository = {
-  findById: async (id: Types.ObjectId | string) => Apartment.findById(id).lean(),
+export const FindApartment_Repository = {
+  findById: async (id: Types.ObjectId | string) => {
+    return await Apartment.findById(id).lean();
+  },
+  findBySocietyIdAndApartmentCode: async (societyId: Types.ObjectId, apartmentCode: string) => {
+    return await Apartment.findOne({ societyId, apartmentCode }).lean();
+  }
 }
