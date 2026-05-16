c8d262e07bed2a7631313b76be3cdc4ea9b4bb7a
ritik1510 <kr.ritik1510@gmail.com>
2026-05-16 12:25:34 +0530

    add update complaint status pipeline route

--- src/routes/Complaint/Complaint.routes.ts
+++ src/routes/Complaint/Complaint.routes.ts
@@ -1,4 +1,4 @@
-import { createComplaint_Controllers, deleteComplaint_Controllers, getComplaint_Controllers, listComplaints_Controllers, updateComplaint_Controllers } from "@/controllers/ComplaintControllers/Complaint.controllers.js";
+import { createComplaint_Controllers, deleteComplaint_Controllers, getComplaint_Controllers, listComplaints_Controllers, updateComplaintStatus_Controllers } from "@/controllers/ComplaintControllers/Complaint.controllers.js";
 import { clerkMiddleware } from "@clerk/express";
 import { Router } from "express";
 
@@ -8,7 +8,7 @@ router.get("/test", (req, res) => {
 }); 
 router.use(clerkMiddleware());
 router.post("/create", createComplaint_Controllers); 
-router.patch("/update", updateComplaint_Controllers)
+router.patch("/:complaintId/status", updateComplaintStatus_Controllers);
 router.get("/get", getComplaint_Controllers); 
 router.get("/get-all-complaint", listComplaints_Controllers); 
 router.delete("/delete/:id", deleteComplaint_Controllers); 
