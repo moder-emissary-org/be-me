5b024b35690222c58babab83c53765d9fd396772
ritik1510 <kr.ritik1510@gmail.com>
2026-05-24 19:35:48 +0530

    Add getNoticesBySocietyId

--- src/repository/NoticesRepository/Notice.repository.ts
+++ src/repository/NoticesRepository/Notice.repository.ts
@@ -1,10 +1,22 @@
 import { Notice } from "@/models/Notice.models.js";
+import { paginate } from "@/Pagination/Pagination.service.js";
 import type { CreateNoticeRepoInput } from "@/services/Notices/Types/Notices.types.js";
+import { GLOBAL_PAGINATION_LIMIT } from "@/utils/utility.js";
 import type { Types } from "mongoose";
 
 export const noticesRepository = {
     create: async (input: CreateNoticeRepoInput) => {
         const doc = await Notice.create(input);
-        return doc; 
-    }
+        return doc;
+    },
+    getNoticesBySocietyId: async (societyId: Types.ObjectId, cursor: string | undefined) => {
+        const result = await paginate({
+            model: Notice,
+            query: { societyId },
+            limit: GLOBAL_PAGINATION_LIMIT,
+            cursor,
+            cursorField: "createdAt"
+        });
+        return result;
+    },
 }
