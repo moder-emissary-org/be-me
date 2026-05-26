8fc424e4bcd49df4ab9486786fa7c98c988df8cb
ritik1510 <kr.ritik1510@gmail.com>
2026-05-26 21:56:26 +0530

    add/seperate the types from the complaints pipeline.

--- src/services/Complaints/Types/Complaints.types.ts
+++ src/services/Complaints/Types/Complaints.types.ts
@@ -1,12 +1,6 @@
 import type { Types } from "mongoose";
 
-export type UpdateComplaintStatusInput = {
-  clerkUserId: string;
-  complaintId: string;
-  status: string;
-  adminRemark?: string;
-};
-
+// main entity interface for complaints
 export interface ComplaintEntity {
   _id: Types.ObjectId;
 
@@ -24,3 +18,27 @@ export interface ComplaintEntity {
 
   adminRemark: null;
 }
+
+// service types
+export type UpdateComplaintStatusInput = {
+  clerkUserId: string;
+  complaintId: string;
+  status: string;
+  adminRemark?: string;
+};
+
+// repository types
+export type CreateComplaintRepositoryInput = Omit<ComplaintEntity, "_id">;
+
+export type UpdateComplaintStatusRepositoryInput = Omit<
+  ComplaintEntity,
+  | "_id"
+  | "title"
+  | "description"
+  | "category"
+  | "societyId"
+  | "apartmentId"
+  | "createdBy"
+> & {
+  complaintId: Types.ObjectId;
+};
