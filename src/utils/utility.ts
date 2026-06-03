d9082d787f63a5d79d541f654c7b24005fbcffb3
ritik1510 <kr.ritik1510@gmail.com>
2026-06-03 15:55:13 +0530

    Add utilities for parsing the query inputs.

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -15,3 +15,15 @@ export const parseCursor = (cursor: unknown): string | undefined => {
   const trimmedCursor = trimString(cursor);
   return trimmedCursor || undefined;
 }
+
+export const parseBooleanQuery = (value: unknown): boolean | undefined => {
+  if (value === "true") return true;
+  if (value === "false") return false;
+
+  return undefined;
+}
+
+export const parseRoleQuery = (value: unknown): "resident" | "guard" | undefined => {
+  if (value === "resident" || value === "guard") return value;
+  return undefined;
+}
